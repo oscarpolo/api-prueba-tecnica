35c33ac13e7ffe76aab9d2e279b63dc5788743d0
oscar <opolo@poligran.edu.co>
2024-05-24 09:45:28 -0500

    fix line

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("dev");
 
-//builder.Services.AddTransient<ConnectionDB>();
+builder.Services.AddTransient<ConnectionDB>();
 
 var app = builder.Build();
 
